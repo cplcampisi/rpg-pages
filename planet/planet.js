48338afeaf464489a10879ba5e506c57a809ff49
Anthony Campisi <acampisi@Anthonys-iMac.local>
2022-02-26 01:43:31 -0700

    planet home page

--- planet/planet.js
+++ planet/planet.js
@@ -10,6 +10,11 @@ const urlParams = new URLSearchParams(urlparam);
 if (urlParams.has('date'))
     date = urlParams.get('date');
 
+function exitbutton()
+{
+    window.location.href = "../gxi50.html"+urlparam+"&skip=1";
+}
+
 /*async function loadTextFiles()
 {
     try 
@@ -47,7 +52,8 @@ async function loadPlanetFile()
       console.error(err);
     }
 
-    ProcessPList();
+    //To provide a slight delay for effect
+    setTimeout(ProcessPList, 1100);
 }
 
 function exitbutton()
@@ -73,10 +79,10 @@ function ProcessPList()
     
     var n = parseInt(lines[0]);
     
-    if (n != lines.length-1)
-        alert ("Planet List Length Mismatch!\nNumber of rows in file: " + parseInt(lines.length-1) + "\nShould be: " + parseInt(n));
+    if (n != lines.length-2)  //(plist.txt has a newline at the end)
+        alert ("Planet List Length Mismatch!\nNumber of rows in file: " + parseInt(lines.length-2) + "\nShould be: " + parseInt(n));
 
-    for (var i = 1; i < lines.length; i++)
+    for (var i = 1; i < lines.length-1; i++)
     {
         var line = lines[i].split(" ");
         var p = new planet(line[1], line[3], line[4], line[2]);
