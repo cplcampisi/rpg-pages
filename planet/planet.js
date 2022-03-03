da450e28193bfb70cccee8dd6efa6ad0981dbb14
Anthony Campisi <acampisi@Anthonys-iMac.local>
2022-03-03 00:25:24 -0700

    quads

--- planet/planet.js
+++ planet/planet.js
@@ -220,53 +220,59 @@ function ProcessQuad(n)
     var pPop = lines[6];
     var pCiv = lines[7];
     
+    var mainText = "QUADRANT STATISTICS:<br><br>";
+    var vecs = vectors.split(" ");
+    mainText += "Vectors (x,y,z):";
+    mainText += vecs[0] +", ";
+    mainText += vecs[1] +", ";
+    mainText += vecs[2] +"<br>";
+    
+    mainText += "Number of planets in quadrant:" + space(4) + nPlanets + "<br>";
+    
     //------------------------------------------------
     // STATS
     //------------------------------------------------
-    var text = "Number of planets:&nbsp;&nbsp;&nbsp;&nbsp;" + nPlanets + "<br><br>";
-    
-    text = text + "Sizes: <br><br>";
+    var sizeText =  "Sizes: <br><br>";
     
     var sizes = pSizes.split(" ");
     //3, 11, 7, 10;
-    text = text + "planetoids:&nbsp;&nbsp;&nbsp;" + sizes[0] + "<br>";
-    text = text + "small:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + sizes[1] + "<br>";
-    text = text + "medium:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + sizes[2] + "<br>";
-    text = text + "large:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + sizes[3] + "<br><br>";
-    
-    var types = pType.split(" ");
+    sizeText = sizeText + "planetoids:&nbsp;&nbsp;&nbsp;" + sizes[0] + "<br>";
+    sizeText = sizeText + "small:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + sizes[1] + "<br>";
+    sizeText = sizeText + "medium:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + sizes[2] + "<br>";
+    sizeText = sizeText + "large:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + sizes[3] + "<br><br>";
     
     //Climate types
-    text = text + "Climate types: <br><br>";
+    var types = pType.split(" ");
+    var climText = "Climate types: <br><br>";
     
-    text = text + "temperate:" + space(3) + types[0] + "<br>";
-    text = text + "ice:" + space(16) + types[1] + "<br>";
-    text = text + "jungle:" + space(10) + types[2] + "<br>";
-    text = text + "water:" + space(11) + types[3] + "<br>";
-    text = text + "mountain:" + space(5) + types[4] + "<br>";
-    text = text + "volcanic:" + space(7) + types[5] + "<br>";
-    text = text + "desert:" + space(10) + types[6] + "<br><br>";
+    climText = climText + "temperate:" + space(3) + types[0] + "<br>";
+    climText = climText + "ice:" + space(16) + types[1] + "<br>";
+    climText = climText + "jungle:" + space(10) + types[2] + "<br>";
+    climText = climText + "water:" + space(11) + types[3] + "<br>";
+    climText = climText + "mountain:" + space(5) + types[4] + "<br>";
+    climText = climText + "volcanic:" + space(7) + types[5] + "<br>";
+    climText = climText + "desert:" + space(10) + types[6] + "<br><br>";
     
     //Populations
     var pops = pPop.split(" ");
-    text = text + "Pop. Density: <br><br>";
+    var popText = "Pop. Density: <br><br>";
     
-    text = text + "minimal:" + space(9) + pops[0] + "<br>";
-    text = text + "sparse:" + space(12) + pops[1] + "<br>";
-    text = text + "moderate:" + space(7) + pops[2] + "<br>";
-    text = text + "dense:" + space(14) + pops[3] + "<br><br>";
+    popText = popText + "minimal:" + space(9) + pops[0] + "<br>";
+    popText = popText + "sparse:" + space(12) + pops[1] + "<br>";
+    popText = popText + "moderate:" + space(7) + pops[2] + "<br>";
+    popText = popText + "dense:" + space(14) + pops[3] + "<br><br>";
     
     //Civilization types
     var civs = pCiv.split(" ");
-    text = text + "Civ. Types: <br><br>";
+    var civText = "Civ. Types: <br><br>";
     
-    text = text + "outpost:" +              space(18) + civs[0] + "<br>";
-    text = text + "industrial:" +           space(14) + civs[1] + "<br>";
-    text = text + "ind./agricultural:" +    space(2) + civs[2] + "<br>";
-    text = text + "agricultural:" +         space(10) + civs[3] + "<br>";
-    text = text + "mercantile:" +           space(12) + civs[4] + "<br>";
-    text = text + "mixed:" +                space(19) + civs[5] + "<br>";
-    text = text + "primitive:" +            space(14) + civs[6] + "<br>";
+    civText = civText + "outpost:" +              space(18) + civs[0] + "<br>";
+    civText = civText + "industrial:" +           space(14) + civs[1] + "<br>";
+    civText = civText + "ind./agricultural:" +    space(2) + civs[2] + "<br>";
+    civText = civText + "agricultural:" +         space(10) + civs[3] + "<br>";
+    civText = civText + "mercantile:" +           space(12) + civs[4] + "<br>";
+    civText = civText + "mixed:" +                space(19) + civs[5] + "<br>";
+    civText = civText + "primitive:" +            space(14) + civs[6] + "<br>";
 
     //------------------------------------------------
     // REGIONS
@@ -290,6 +296,48 @@ function ProcessQuad(n)
         var r = new region(name, rName, title, type, lName);
         rList.push(r);
     }
+    
+    document.getElementById("qtitle").innerHTML = "QUADRANT " + romanNumeral(n);
+    document.getElementById("qmain").innerHTML = mainText;
+    document.getElementById("qsizes").innerHTML = sizeText;
+    document.getElementById("qtypes").innerHTML = climText;
+    document.getElementById("qpops").innerHTML = popText;
+    document.getElementById("qcivs").innerHTML = civText;
+    
+    document.getElementById("qfoot").innerHTML = "QUADRANT " + romanNumeral(n);
+}
 
+function romanNumeral(n)
+{
+    var numeral = "";
+    switch(n)
+    {
+        case 1:
+            numeral = "I";
+        break;
+        case 2:
+            numeral = "II";
+        break;
+        case 3:
+            numeral = "III";
+        break;
+        case 4:
+            numeral = "IV";
+        break;
+        case 5:
+            numeral = "V";
+        break;
+        case 6:
+            numeral = "VI";
+        break;
+        case 7:
+            numeral = "VII";
+        break;
+        case 8:
+            numeral = "VIII";
+        break;
+    }
+    
+    return numeral;
 }
 
