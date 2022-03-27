8635982b94553b0346e089523c8d5525a28d7891
Anthony Campisi <acampisi@Anthonys-iMac.local>
2022-03-27 16:24:49 -0600

    uargos restricted login

--- uargos/uargos.js
+++ uargos/uargos.js
@@ -549,6 +549,7 @@ document.onkeydown = function(event)
                 if (newChar == 82) //R
                 {
                     option="";
+                    textfield = 0;
                     OptionInput("xpassword");
                     return;
                 }
