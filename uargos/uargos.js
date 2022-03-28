6b8e8f2bb9a3e6aaa08a383b6b7b679a3f702ccb
Tonyers <tony@eventrentalsystems.com>
2022-03-27 22:02:43 -0600

    uargos restricted select

--- uargos/uargos.js
+++ uargos/uargos.js
@@ -1016,6 +1016,7 @@ async function LoadRestricted()
 }
 function ProcessRestricted(paperTxt)
 {
+    document.getElementById("xlist").innerHTML="";
     papers = [];
     var lines;
     lines = paperTxt.split(/(?:\r\n|\r|\n)/g);
