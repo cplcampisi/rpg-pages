7529e8f27b6ee344dfa47c7e943020c1b71b13f6
Tonyers <tony@eventrentalsystems.com>
2022-04-18 09:12:51 -0600

    Adjusting volume down in gxi50 (new method)

--- gxi.js
+++ gxi.js
@@ -28,7 +28,7 @@ function beep(volume, frequency, type, duration)
 // VIEW0 (INTRO)
 //*************************************************************************
 //*************************************************************************
-var vol = 0.1;
+var vol = 0;
 var ready;
 var view = 0;
 var urlparam = window.location.search;
