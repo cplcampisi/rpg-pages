ffaa1d9c18d35044194b3fd1daf0d24348b70475
Tonyers <tony@eventrentalsystems.com>
2022-04-17 23:49:39 -0600

    Adjusting volume down, gxi50 and planet intro

--- gxi.js
+++ gxi.js
@@ -27,7 +27,7 @@ function beep(volume, frequency, type, duration)
 // VIEW0 (INTRO)
 //*************************************************************************
 //*************************************************************************
-var vol = 0.3;
+var vol = 0.1;
 var ready;
 var view = 0;
 var urlparam = window.location.search;
