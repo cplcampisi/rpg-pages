374896e95ea7dae29292e37c9faf9967ecaacfc5
Tonyers <tony@eventrentalsystems.com>
2022-09-03 00:27:55 -0600

    Trying to adjust volume on gxi50 tones

--- gxi.js
+++ gxi.js
@@ -6,7 +6,9 @@ function beep(volume, frequency, type, duration)
 
     oscillator.connect(gainNode);
 
-    gainNode.gain.value = 1;//volume;
+    gainNode.gain.value = 0.01;//volume;
+    console.log("gain:");
+    console.log(gainNode.gain.value);
     
     //gainNode.gain.setValueAtTime(volume, audioCtx.currentTime);
 
